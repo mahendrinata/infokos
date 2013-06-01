5eba4f5d7840d0f97135094214dad343a9c30e41
Greg Molnar <molnargerg@gmail.com>
2013-06-01 21:53:03 +0300

    mysql inject issue

--- admin/modul/pelanggan/aksi.php
+++ admin/modul/pelanggan/aksi.php
@@ -4,11 +4,11 @@ session_start();
 include "../../../config/koneksi.php";
 
 if ($_GET[act] == 'delete') {
-    mysql_query("DELETE FROM pelanggan WHERE id = '$_GET[id]'");
+    mysql_query("DELETE FROM pelanggan WHERE id = ".(int)$_GET[id]);
 } elseif ($_GET[act] == 'n_aktif') {
-    mysql_query("UPDATE pelanggan SET aktif = 0 WHERE id = '$_GET[id]'");
+    mysql_query("UPDATE pelanggan SET aktif = 0 WHERE id = ".(int)$_GET[id]);
 } elseif ($_GET[act] == 'aktif') {
-    mysql_query("UPDATE pelanggan SET aktif = 1 WHERE id = '$_GET[id]'");
+    mysql_query("UPDATE pelanggan SET aktif = 1 WHERE id = ".(int)$_GET[id]);
 }
 
 header("Location:../../media.php?page=pelanggan&p=".$_SESSION['admin']['pagination']['pelanggan']);
